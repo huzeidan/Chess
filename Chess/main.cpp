da9fd98ccd42c878232c743928bc79c28a72b9f8
huzeidan <huzeidan@umich.edu>
2021-02-16 14:04:53 -0500

    Initial Commit

---
+++ Chess/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chess
+//
+//  Created by Husam Zeidan on 2/16/21.
+//  Copyright © 2021 Husam Zeidan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
